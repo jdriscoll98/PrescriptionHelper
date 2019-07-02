a56360d1d8576f65fcda431a6c000d7a77f5803c
jdriscoll98 <41447166+jdriscoll98@users.noreply.github.com>
2019-07-01 21:21:09 -0400

    first page stage 1 html

--- website/urls.py
+++ website/urls.py
@@ -1,15 +1,12 @@
 from django.conf.urls import url, include
 from django.urls import path
-from .views import shifts_available_page_view
 from . import views
+from .views import *
 # Application Routes (URLs)
 
 app_name = 'website'
 
 urlpatterns = [
     	# General Page Views
-		url(r'^$', views.homepage_view, name='homepage_view'),
-		url(r'^post-shift/(?P<pk>\d+)$', views.PostShiftView.as_view(), name='post_shift'),
-		url(r'^shifts-available$', views.shifts_available_page_view, name='ShiftsAvailableView'),
-		url(r'^pick-up-shift/(?P<pk>\d+)$', views.pick_up_shift_view, name='pick_up_shift'),
+		url(r'^$', HomePageView.as_view(), name='homepage'),
 ]
